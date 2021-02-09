2ccdf3d9c55c234ab93a3126e5443e4cc68246b2
Pjotr Prins <pjotr.public01@thebird.nl>
2021-02-09 13:10:24 +0000

    MARIABACKUP_TUX01

--- scripts/tux01/backup.sh
+++ scripts/tux01/backup.sh
@@ -23,16 +23,16 @@ date
 # sheepdog_run.rb -c "$borg create  --stats /export/backup/borg-etc::P2_etc-$stamp /etc" --tag 'BORG-TUX01-ETC' --log
 
 tag=RSYNC_REDIS_TUX01_P2
-sheepdog_run.rb -c 'rsync -va /home/redis/ --rsync-path=/usr/bin/rsync -e "ssh -i /home/mariadb/.ssh/id_backup" wrk@penguin2.genenetwork.org:/export/backup/tux01-redis/' --always -v --tag $tag
+sheepdog_run.rb -h rabbit -c 'rsync -va /home/redis/ --rsync-path=/usr/bin/rsync -e "ssh -i /home/mariadb/.ssh/id_backup" wrk@penguin2.genenetwork.org:/export/backup/tux01-redis/' --always -v --tag $tag
 
 tag=RSYNC_GN2_PROD_TUX01_P2
-sheepdog_run.rb -c 'rsync -va /home/gn2/production/ --exclude tmp/ --rsync-path=/usr/bin/rsync -e "ssh -i /home/mariadb/.ssh/id_backup" wrk@penguin2.genenetwork.org:/export/backup/tux01-gn2/' --always -v --tag $tag
+sheepdog_run.rb -h rabbit -c 'rsync -va /home/gn2/production/ --exclude tmp/ --rsync-path=/usr/bin/rsync -e "ssh -i /home/mariadb/.ssh/id_backup" wrk@penguin2.genenetwork.org:/export/backup/tux01-gn2/' --always -v --tag $tag
 
 tag=RSYNC_MARIADB_TUX01_P2
-sheepdog_run.rb -c 'rsync -va /var/lib/mysql/* --rsync-path=/usr/bin/rsync -e "ssh -i /home/mariadb/.ssh/id_backup" wrk@penguin2.genenetwork.org:/export/backup/tux01-mariadb/' --always -v --tag $tag
+sheepdog_run.rb -h rabbit -c 'rsync -va /var/lib/mysql/* --rsync-path=/usr/bin/rsync -e "ssh -i /home/mariadb/.ssh/id_backup" wrk@penguin2.genenetwork.org:/export/backup/tux01-mariadb/' --always -v --tag $tag
 
 tag=RSYNC_MARIADB_TUX01_P2_NEW
-sheepdog_run.rb -c 'rsync -va /home/backup/tux01_mariadb_new --rsync-path=/usr/bin/rsync -e "ssh -i /home/mariadb/.ssh/id_backup" wrk@penguin2.genenetwork.org:/export/backup/tux01-mariadb-new' --always -v --tag $tag
+sheepdog_run.rb -h rabbit -c 'rsync -va /home/backup/tux01_mariadb_new --rsync-path=/usr/bin/rsync -e "ssh -i /home/mariadb/.ssh/id_backup" wrk@penguin2.genenetwork.org:/export/backup/tux01-mariadb-new' --always -v --tag $tag
 
 # chown pjotr.pjotr -R /export/backup/
 # chown pjotr.pjotr /export/backup
--- scripts/tux01/backup_mariadb.sh
+++ scripts/tux01/backup_mariadb.sh
@@ -7,29 +7,14 @@ if [ "$EUID" -eq 0 ]
 fi
 
 # ---- for sheepdog
+source $(dirname "$0")/sheepdog_env.sh
 
-export GEM_PATH=/home/pjotr/opt/deploy/lib/ruby/vendor_ruby  
-export PATH=/home/pjotr/iwrk/deploy/deploy/bin:/home/pjotr/opt/deploy/bin:$PATH                                                                                                                                       
 backupdir=/home/backup/tux01_mariadb_new
-# test: su mysql -c mariabackup --backup --target-dir=$backupdir/latest/ --user=webqtlout --password=webqtlout
-rm -rf $backupdir/latest
+# rm -rf $backupdir/latest
 tag="MARIABACKUP_TUX01"
-sheepdog_run.rb --always -v --tag $tag -c "mariabackup --backup --target-dir=$backupdir/latest/ --user=webqtlout --password=webqtlout"
+sheepdog_run.rb -h rabbit --always -v --tag $tag -c "mariabackup --backup --target-dir=$backupdir/latest/ --user=webqtlout --password=webqtlout"
 tag="MARIABACKUP_PREPARE_TUX01"
-sheepdog_run.rb --always -v --tag $tag -c "mariabackup --prepare --target-dir=$backupdir/latest/" 
-
-borg=/home/pjotr/opt/borg/bin/borg
-# . /home/wrk/.borg-pass
-export stamp=$(date +%A-%Y%m%d-%H:%M:%S)
-echo $stamp
-echo "Starting $0"
-date
-
-# echo "Backup /etc"
-# # sheepdog_run.rb -c "$borg create  --stats /export/backup/borg-etc::P2_etc-$stamp /etc" --tag 'BORG-TUX01-ETC' --log
-
-# test: su mysql -c "$borg create $borgdir::TUX01_Mariadb-$stamp $backupdir --progress"
-
-borgdir=/export/backup/borg/borg-tux01
+sheepdog_run.rb -h rabbit --always -v --tag $tag -c "mariabackup --prepare --target-dir=$backupdir/latest/" 
+borgdir=/export2/backup/borg/borg-tux01
 tag="BORG-TUX01-MARIADB"
-sheepdog_run.rb --always -v --tag $tag -c "$borg create  --stats $borgdir::TUX01_Mariadb-$stamp $backupdir"
+sheepdog_borg.rb -h rabbit --always -v --tag $tag -b $borgdir $backupdir --args '--stats'
