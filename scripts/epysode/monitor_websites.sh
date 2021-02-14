2f0dc6e1d4705868f57e4e6e4fceff05986ab86a
Pjotr Prins <pjotr.public01@thebird.nl>
2021-02-14 15:12:12 +0100

    Moved to https

--- scripts/epysode/monitor_websites.sh
+++ scripts/epysode/monitor_websites.sh
@@ -30,20 +30,20 @@ echo $TAG
 sheepdog_run.rb -c "curl $URL|grep $FIND" --tag $TAG $*
 
 TAG=WEB_BNW
-URL="http://bnw.genenetwork.org/sourcecodes/home.php"
+URL="https://bnw.genenetwork.org/sourcecodes/home.php"
 FIND=BNW
 echo $TAG
 sheepdog_run.rb -c "curl $URL|grep $FIND" --tag $TAG $*
 
 
 TAG=WEB_RATS_PUB
-URL="--connect-timeout 30 --retry 3 --retry-delay 5 http://rats.pub/"
+URL="--connect-timeout 30 --retry 3 --retry-delay 5 https://rats.pub/"
 FIND=Addiction
 echo $TAG
 sheepdog_run.rb -c "curl $URL|grep $FIND" --tag $TAG $*
 
 TAG=WEB_PUBSEQ
-URL=http://covid19.genenetwork.org/
+URL=https://covid19.genenetwork.org/
 FIND=PubSeq
 echo $TAG
 sheepdog_run.rb -c "curl $URL|grep -i $FIND" --tag $TAG $*
@@ -61,13 +61,13 @@ echo $TAG
 sheepdog_run.rb -c "curl $URL|grep -i $FIND" --tag $TAG $*
 
 TAG=WEB_HEGP
-URL=http://hegp.genenetwork.org/
+URL=https://hegp.genenetwork.org/
 FIND=hegp
 echo $TAG
 sheepdog_run.rb -c "curl $URL|grep -i $FIND" --tag $TAG $*
 
 TAG=WEB_GITEA
-URL=http://git.genenetwork.org/
+URL=https://git.genenetwork.org/
 FIND=Gitea
 echo $TAG
 sheepdog_run.rb -c "curl $URL|grep -i $FIND" --tag $TAG $*
