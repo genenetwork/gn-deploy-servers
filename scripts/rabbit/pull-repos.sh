9f94ec2851d6db2a59d6741557fd07c0170aeecc
Pjotr Prins <pjotr.public01@thebird.nl>
2021-02-21 04:08:06 -0600

    rabbit

--- scripts/rabbit/pull-repos.sh
+++ scripts/rabbit/pull-repos.sh
@@ -1,10 +1,7 @@
 #! /bin/bash
 
-echo "Starting $0"
-date
-# tux01 is coming from penguin2 anyway!
-# echo "rsync from Tux01"
-# rsync -vaz --delete -e "ssh -i $HOME/.ssh/id_rabbit_backup" pjotr@tux01:/home/mariadb/database /export/backup/tux01/
-echo "rsync from Penguin2"
-rsync -vaz --delete -e "ssh -i $HOME/.ssh/id_rabbit_backup" penguin2.genenetwork.org:/export/backup/ /export/backup/penguin2/
+# ---- for sheepdog
+source $(dirname "$0")/sheepdog_env.sh
+
+sheepdog_rsync.rb --tag RABBIT_RSYNC_P2 wrk@penguin2.genenetwork.org:/export/backup/ /export/backup/penguin2/ --always -v --args '-e "ssh -i /home/wrk/.ssh/id_rabbit_backup"'
 
--- scripts/rabbit/push-to-epysode.sh
+++ scripts/rabbit/push-to-epysode.sh
@@ -1,7 +1,17 @@
 #! /bin/bash
 
-echo "Starting $0"
-date
-echo "rsync to epysode"
-rsync -vaz --delete -e "ssh -i $HOME/.ssh/id_rabbit_backup" /export/backup/ epysode.borm.org:/export/backup/rabbit/
+# ---- for sheepdog
+source $(dirname "$0")/sheepdog_env.sh
+
+RDIR=$HOME/mnt
+
+mkdir -p $RDIR
+sheepdog_run.rb --tag MOUNT -c "/usr/bin/sshfs -o IdentityFile=/home/wrk/.ssh/id_rabbit_backup \
+  ibackup@epysode.borm.org:/ $RDIR" -v --always
+sleep 1
+sheepdog_run.rb --tag TEST -c "ls -l $RDIR" --always -v
+sheepdog_rsync.rb --tag BACCHUS_RSYNC_EPI /export/backup/ $RDIR/rabbit/  --always -v
+sheepdog_run.rb --tag UNMOUNT -c "/bin/fusermount -u $RDIR" -v --always
+
+# rsync -vaz --delete -e "ssh -i $HOME/.ssh/id_rabbit_backup" /export/backup/ epysode.borm.org:/export/backup/rabbit/
 
